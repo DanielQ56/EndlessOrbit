d87e865e4183acfd334b664c46b4bb145d454cc8
Danny Qi <drqi@uci.edu>
2019-05-09 21:19:03 -0700

    PlayerController and Planet script

---
+++ EndlessOrbit/Assets/Scripts/CelestialBodies/CelestialBody.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelestialBody : MonoBehaviour
+{
+    [SerializeField] float minRadius;
+    [SerializeField] float maxRadius;
+    [SerializeField] Sprite[] sprites;
+
+    float radius;
+    CircleCollider2D m_collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = Random.Range(minRadius, maxRadius);
+        m_collider = GetComponent<CircleCollider2D>();
+        m_collider.radius = radius;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerController>().NewBodyToOrbit(transform);
+        }
+    }
+}
---
+++ EndlessOrbit/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] int velocity;
+    [SerializeField] float rotationAngle;
+    [SerializeField] Transform initialBody;
+    [SerializeField] float MaxSpeed;
+
+    int direction = 1;
+
+    bool tethered = true;
+
+    Transform BodyToRotateAround;
+
+    Vector3 posToMoveTowards;
+
+    PlayerState state;
+
+    public enum PlayerState
+    {
+        Tethered,
+        Free
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BodyToRotateAround = initialBody;
+        state = PlayerState.Tethered;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && state == PlayerState.Tethered)
+        {
+            Detach();
+        }
+        Move();
+    }
+
+    void Move()
+    {
+        if (state == PlayerState.Tethered)
+        {
+            MoveAroundBody();
+        }
+        else if(state == PlayerState.Free)
+        {
+            MoveStraight();
+        }
+    }
+
+    void Detach()
+    {
+        state = PlayerState.Free;
+        Vector3 relativePosition = new Vector3(this.transform.position.x - BodyToRotateAround.transform.position.x,
+            this.transform.position.y - BodyToRotateAround.transform.position.y, 0);
+        float inverseSlope = -1 / (relativePosition.y / relativePosition.x);
+        float b = relativePosition.y - (relativePosition.x * inverseSlope);
+        float x = relativePosition.x - Mathf.Sign(relativePosition.y) * velocity;
+        posToMoveTowards = new Vector3(x, (inverseSlope * x + b), 0);
+    }
+
+    void MoveStraight()
+    {
+         transform.Translate( Vector3.ClampMagnitude(posToMoveTowards, MaxSpeed) * Time.deltaTime, BodyToRotateAround.transform);
+    }
+
+    void MoveAroundBody()
+    {
+        this.transform.RotateAround(BodyToRotateAround.position, Vector3.forward, rotationAngle * Time.deltaTime);
+    }
+
+    public void NewBodyToOrbit(Transform newBody)
+    {
+        //direction = (transform.position.x < newBody.transform.position.x ? -1 : 1);
+        //rotationAngle = Mathf.Abs(rotationAngle) * direction;
+        BodyToRotateAround = newBody;
+        state = PlayerState.Tethered;
+    }
+}
