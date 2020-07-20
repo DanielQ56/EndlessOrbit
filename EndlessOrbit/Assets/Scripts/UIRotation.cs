2b6990447a7e9adc6707004805a0ec43a5313e1e
tworii <timq720@gmail.com>
2020-07-19 19:38:42 -0700

    Added BKGDs to parallax and UI rotation script

---
+++ EndlessOrbit/Assets/Scripts/UIRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRotation : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Vector3 direction;
+
+    [SerializeField]
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        direction = new Vector3(0, 0, speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.Rotate(direction);
+    }
+}
