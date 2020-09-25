488a2926e8c4922e292968686b379fdd50f1df9e
Daniel Qi <drqi@gmail.com>
2020-09-24 20:17:16 -0700

    Updated Change Mode panel to have selected sprite

---
+++ EndlessOrbit/Assets/Scripts/Menu/Selectable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Selectable : MonoBehaviour
+{
+    [SerializeField] GameObject SelectedOverlay;
+
+    Button b;
+
+    SelectableParent sp;
+
+    private void Awake()
+    {
+        b = this.GetComponent<Button>();
+        sp = GetComponentInParent<SelectableParent>();
+        b.onClick.AddListener(Clicked);
+    }
+
+    void Clicked()
+    {
+        sp.ButtonSelected(this);
+    }
+
+    public void Selected()
+    {
+        if(SelectedOverlay != null)
+            SelectedOverlay.SetActive(true);
+    }
+
+    public void Deselected()
+    {
+        if (SelectedOverlay != null)
+            SelectedOverlay.SetActive(false);
+    }
+
+
+
+    private void OnDestroy()
+    {
+        b.onClick.RemoveListener(Clicked);
+    }
+}
---
+++ EndlessOrbit/Assets/Scripts/Menu/SelectableParent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectableParent : MonoBehaviour
+{
+    [SerializeField] Selectable[] sel; 
+
+
+    public void ButtonSelected(Selectable s)
+    {
+        foreach(Selectable selected in sel)
+        {
+            selected.Deselected();
+        }
+        s.Selected();
+
+    }
+}
